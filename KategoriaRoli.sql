fa7f5b99a4e0e2d32adab849c0820dfe7c3f19ab
rinorselmani9 <rs44583@ubt-uni.net>
2022-05-13 20:32:04 +0200

    commit 10

---
+++ KategoriaRoli.sql
@@ -0,0 +1,10 @@
+create table dbo.KategoriaRoli(
+KategoriaId int identity (1,1),
+KategoriaName varchar(500)
+)
+
+
+select * from dbo.KategoriaRoli
+
+insert into dbo.KategoriaRoli values 
+('Support')
