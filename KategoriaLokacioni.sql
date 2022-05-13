b5694234a726ea379ce4f1aa828658a7ef4a4a6f
ak47385 <77011551+ak47385@users.noreply.github.com>
2022-05-13 17:28:46 +0200

    commit 9

---
+++ KategoriaLokacioni.sql
@@ -0,0 +1,10 @@
+create table dbo.KategoriaLokacioni(
+KategoriaId int identity (1,1),
+KategoriaName varchar(500)
+)
+
+
+select * from dbo.KategoriaLokacioni
+
+insert into dbo.KategoriaLokacioni values 
+('Support')
