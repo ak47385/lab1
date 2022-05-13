78d09ef9b700e00362830318bc39751cb914ea1e
ak47385 <77011551+ak47385@users.noreply.github.com>
2022-05-13 17:17:21 +0200

    commit 5

---
+++ KategoriaCertifikata.sql
@@ -0,0 +1,12 @@
+create table dbo.KategoriaCer(
+KategoriaId int identity (1,1),
+KategoriaName varchar(500)
+)
+
+
+select * from dbo.KategoriaCer
+
+insert into dbo.KategoriaCer values 
+('Anila')
+
+drop table dbo.KategoriaCertifikata
