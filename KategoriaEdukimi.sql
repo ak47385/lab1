5ed704581645539d6034dd8bc116413437ca5bf6
ak47385 <77011551+ak47385@users.noreply.github.com>
2022-05-13 17:19:45 +0200

    commit 6

---
+++ KategoriaEdukimi.sql
@@ -0,0 +1,10 @@
+create table dbo.KategoriaEdukimi(
+KategoriaId int identity (1,1),
+KategoriaName varchar(500)
+)
+
+
+select * from dbo.KategoriaEdukimi
+
+insert into dbo.KategoriaEdukimi values 
+('Shkollimi Fillor')
