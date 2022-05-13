ed219bfad766b16f2ec8e5e31f13fccdfba9d2bc
ak47385 <77011551+ak47385@users.noreply.github.com>
2022-05-13 17:25:05 +0200

    commit 7

---
+++ KategoriaEvent.sql
@@ -0,0 +1,14 @@
+create table dbo.Kategoria(
+KategoriaId int identity (1,1),
+KategoriaName varchar(500)
+)
+
+
+select * from dbo.Kategoria
+
+insert into dbo.Kategoria values 
+('Support')
+
+use EmployeeDB
+
+drop table dbo.Department
