cf0514616f54216b86c4247a79dda2e251ef4d7b
rinorselmani9 <rs44583@ubt-uni.net>
2022-05-13 20:17:25 +0200

    comit 10

---
+++ KategoriaPost.sql
@@ -0,0 +1,10 @@
+create table dbo.KategoriaPost(
+KategoriaId int identity (1,1),
+KategoriaName varchar(500)
+)
+
+
+select * from dbo.KategoriaPost
+
+insert into dbo.KategoriaPost values 
+('Support')
