1f3de3a893fc80ccef4c89e8c127383c7458483c
rinorselmani9 <rs44583@ubt-uni.net>
2022-05-14 09:20:58 +0200

    commit 12

---
+++ KategoriaSkills.sql
@@ -0,0 +1,10 @@
+create table KategoriaSkills(
+	KategoriaId int identity (1,1),
+	KategoriaName varchar(500)
+)
+
+insert into KategoriaSkills values ('Kreativitet')
+
+select *
+from KategoriaSkills
+
